405f577173533fe0e7a9659de0b98da7da5b200f
rob alexander <itsticksco@gmail.com>
2018-05-15 08:41:37 +0100

    reversed pixels are cool

--- play.js
+++ play.js
@@ -17,7 +17,7 @@ video.autoplay=true;
 video.muted=true;
 video.playbackRate=2;
 video.crossOrigin = "Anonymous";
-video.src= 'https://doc-04-1k-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/jg74n3fnnn6pi70fk51dqgosm13d5a2a/1526313600000/01524003728762920652/*/1ZwhE85SPCAo6U5A6aC7D6spHviLSgSBm?e=download';
+video.src= 'https://doc-04-1k-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/n4u8m38t0t9i26mq4cspkgrf2slefdcu/1526364000000/01524003728762920652/*/1ZwhE85SPCAo6U5A6aC7D6spHviLSgSBm?e=download';
 // https://drive.google.com/uc?export=download&id=1ZwhE85SPCAo6U5A6aC7D6spHviLSgSBm
 
 var canvas = document.createElement('canvas');
@@ -108,10 +108,12 @@ ImageData.prototype.greenScramble = function(){
 }
 
 ImageData.prototype.reversePixels = function(){
-	var buf8 = new Uint8ClampedArray(this.data);
+	this.data=this.data.reverse();
+	return this;
+}
 
-
-	this.data.set(buf8.reverse());
+ImageData.prototype.sortPixels = function(){
+	this.data=this.data.sort();
 	return this;
 }
 
@@ -138,7 +140,7 @@ var step = function(){
 
 video.onended = function(){
 	frames = frames.map(function(f,i){
-	 return (i%2==0) ? f.greenScramble() : f.reversePixels();
+	 return (i%2==0) ? f.reversePixels() : f;
 	//return f.invert();
 	})
 	// .map(function(f,i){
